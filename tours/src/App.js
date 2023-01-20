2b7a2761877496c65c4967bd0ae4b7188f4ae658
hopeVaughn <hopevaughnwarren@gmail.com>
2023-01-20 09:46:08 -0800

    add basic jsx for app

--- tours/src/App.js
+++ tours/src/App.js
@@ -7,6 +7,24 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
   if (loading) {
     return (
       <main>
@@ -16,7 +34,7 @@ function App() {
   }
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} />
     </main>
   )
 }
