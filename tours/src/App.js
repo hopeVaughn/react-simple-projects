0ee1f3c244eaffb57b42069963be017c7561c701
hopeVaughn <hopevaughnwarren@gmail.com>
2023-01-20 10:01:08 -0800

    finish components logic for project

--- tours/src/App.js
+++ tours/src/App.js
@@ -7,7 +7,10 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
-
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
   const fetchTours = async () => {
     setLoading(true);
     try {
@@ -32,9 +35,19 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button onClick={fetchTours} className="btn">
+          refresh
+        </button>
+      </div>
+    </main>
+  }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   )
 }
